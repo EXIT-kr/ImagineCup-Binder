b88e2791b441ce4a6a2b1f3667aeb2fb445f7485
nayunhwan <kbk9288@gmail.com>
2017-01-01 19:49:34 +0900

    [v0.0.1] Edit Sprint vertical-align

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
   $("#leftPanel").load("_includes/left.html");
   // $("#rightPanel").load("route/chat.html");
-  $("#rightPanel").load("route/sprint.html");
+  $("#rightPanel").load("route/dashBoard.html");
 });
