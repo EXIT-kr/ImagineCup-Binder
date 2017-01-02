8dfc9bd89acd8a3950ce1e053ba0bb0aa611c76f
nayunhwan <kbk9288@gmail.com>
2017-01-02 19:37:51 +0900

    [v0.0.2] fullCalendar 만드는 중...

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,16 @@
 $(function(){
   $("#leftPanel").load("_includes/left.html");
-  $("#rightPanel").load("route/dashBoard.html");
   // $("#rightPanel").load("route/chat.html");
   // $("#rightPanel").load("route/sprint.html");
+  $("#rightPanel").load("route/dashBoard.html");
 });
+
+$(document).ready(function(){
+  // $("#leftPanel").load("_includes/left.html");
+  // $("#rightPanel").load("route/dashBoard.html");
+  $('#calendar').fullCalendar({
+    dayClick: function() {
+        alert('a day has been clicked!');
+    }
+  });
+})
