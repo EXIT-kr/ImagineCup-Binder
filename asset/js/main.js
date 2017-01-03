28ee4f799f5f8fad47b0ac250a008e82fd65b916
nayunhwan <kbk9288@gmail.com>
2017-01-03 15:21:44 +0900

    [v0.0.3] Edit chatPanel

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
+
+
   $("#topPanel").load("_includes/top.html");
   $("#leftPanel").load("_includes/left.html");
-  $("#rightPanel").load("route/dashBoard.html");
+  $("#rightPanel").load("route/chat.html");
   $("#bottomPanel").load("_includes/bottom.html");
   $('#calendar').fullCalendar({
     dayClick: function() {
         alert('a day has been clicked!');
     }
   });
+
+  $('#btn_dashBoard').click(function(){
+      console.log("dashBoard");
+      $("#rightPanel").load("route/dashBoard.html");
+  })
+
+  $('#btn_chat').click(function(){
+    console.log("chat");
+    $("#rightPanel").load("route/chat.html");
+  })
+
+  $('#btn_calendar').click(function(){
+    console.log("carlendar");
+    $("#rightPanel").load("route/sprint.html");
+  })
 })
