7aaaa57c9ac4630338a7fc55285de9ae5ff15445
nayunhwan <kbk9288@gmail.com>
2017-01-03 11:56:36 +0900

    [v0.0.3] Divide BottomPanel

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#topPanel").load("_includes/top.html");
   $("#leftPanel").load("_includes/left.html");
   $("#rightPanel").load("route/dashBoard.html");
+  $("#bottomPanel").load("_includes/bottom.html");
   $('#calendar').fullCalendar({
     dayClick: function() {
         alert('a day has been clicked!');
