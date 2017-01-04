94bba8209178d169ddaaeb8533af80362cc5c7cb
nayunhwan <kbk9288@gmail.com>
2017-01-04 16:52:38 +0900

    [v0.0.3] Edit javsscript

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,15 +4,15 @@ function loadPage(id ,url){
     type: "GET",
     success: function(result){}
   }).done(function(result){
+    console.log("hello");
     $(id).html(result);
   });
 }
 
 $(document).ready(function(){
-
+  loadPage("#topPanel", "_includes/top.html");
   loadPage("#leftPanel", "_includes/left.html");
   loadPage("#rightPanel", "route/sprint.html")
-  loadPage("#topPanel", "_includes/top.html");
   loadPage("#bottomPanel", "_includes/bottom.html");
 
 })
