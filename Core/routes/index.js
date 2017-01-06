089521ef08f362539eff33c6cf91180b4cd98d01
nayunhwan <kbk9288@gmail.com>
2017-01-06 17:45:02 +0900

    [v0.0.4] Edit PageHeader more flexible

--- Core/routes/index.js
+++ Core/routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DashBoard' });
 });
 
 router.get('/chat', function(req, res, next) {
-  res.render('chat', { title: 'Express' });
+  res.render('chat', { title: 'Chat' });
 });
 
 router.get('/agenda', function(req, res, next) {
-  res.render('agenda', { title: 'Express' });
+  res.render('agenda', { title: 'Agenda' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
