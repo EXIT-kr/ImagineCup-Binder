1783daf132d1c8d4f3ee4a2aa30f646d51cd66c9
nayunhwan <kbk9288@gmail.com>
2017-01-06 12:35:43 +0900

    [v0.0.4] Node.js 서버 라우팅 작업 중...

--- Core/app.js
+++ Core/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var route_login = require('./routes/login');
 
 var app = express();
 
@@ -22,8 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Route
 app.use('/', index);
+app.use('/login', route_login);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
