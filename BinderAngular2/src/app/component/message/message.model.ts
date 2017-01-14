1a242e42c69042ac05f0b7b06d131412ab4de239
윤 지우 <wldnthsus@icloud.com>
2017-01-14 22:49:58 +0900

    [v0.0.5] Add Modeled Chat Data

--- BinderAngular2/src/app/component/message/message.model.ts
+++ BinderAngular2/src/app/component/message/message.model.ts
@@ -1,12 +1,18 @@
 export class Message{
-  type:string;
-  name:string;
-  date:string;
-  text:string;
-  constructor(type:string,name:string,date:string,text:string){
+  type : string;
+  chat_id:string;
+  sender_id:string;
+  receiver_id:string;
+  chat_words:string;
+  chat_time:string;
+  chat_read:boolean;
+  constructor(type: string, chat_id:string, sender_id:string, receiver_id:string, chat_words:string, chat_time:string, chat_read:boolean){
     this.type = type;
-    this.name = name;
-    this.date = date;
-    this.text = text;
+    this.chat_id = chat_id;
+    this.sender_id = sender_id;
+    this.receiver_id = receiver_id;
+    this.chat_words = chat_words;
+    this.chat_time= chat_time;
+    this.chat_read= chat_read;
   }
 }
