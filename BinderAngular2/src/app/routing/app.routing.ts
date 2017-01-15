3c006f859e1ada95c992445f043754ba48c91784
nayunhwan <kbk9288@gmail.com>
2017-01-15 20:46:33 +0900

    [v0.0.5] Add Route Comment

--- BinderAngular2/src/app/routing/app.routing.ts
+++ BinderAngular2/src/app/routing/app.routing.ts
@@ -2,8 +2,14 @@ import {Routes,RouterModule} from "@angular/router";
 import {AgendaComponent,ChatComponent,DashComponent,LoginComponent} from "app/main";
 
 
-
+// Route
 const APP_ROUTES: Routes = [
+
+  /*
+    Path : URL 뒤에 붙는 부분
+    해당 URL이 왔을 때 어떤 Component로 연결할지 정의를 해준다.
+  */
+
   { path:'agenda',component:AgendaComponent},
   { path:'chat',component:ChatComponent},
   { path:'dash',component:DashComponent}
